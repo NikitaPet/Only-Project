313ece17015c6b885b7b93d1d4b0e721a9fc81b9
Nikita-Pet <nikita_petrishchev94@mail.ru>
2024-09-22 04:50:00 +0300

    dev#1, project setting

---
+++ src/app/App.tsx
@@ -0,0 +1,6 @@
+import '@global/styles/global-styles.scss'
+import styles from './styles/App.module.scss'
+
+export default function App() {
+    return <div className={styles.wrapper}>App</div>
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import App from 'app'
+
+const container = document.getElementById('root') as Element
+export const root = ReactDOM.createRoot(container)
+root.render(<App />)
